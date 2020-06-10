e8fd3d3bb408cbfe6be8e2bb337487bfe6936d8b
Sarthak Mittal <sarthakmittal2608@gmail.com>
2020-06-11 00:10:10 +0530

    Set default use_sime_time value to false
    Signed-off-by: Sarthak Mittal <sarthakmittal2608@gmail.com>

--- ros/launch/orb_slam2_d435_rgbd_launch.py
+++ ros/launch/orb_slam2_d435_rgbd_launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'use_sim_time',
-            default_value='true',
+            default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 
         DeclareLaunchArgument(
